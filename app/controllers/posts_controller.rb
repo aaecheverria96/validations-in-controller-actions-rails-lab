73def653252f0188c1825dad22fb02646b48f1b3
Your Username <you@example.com>
2021-03-27 18:52:15 -0400

    commit tree 1779e7b6aa0a8487e11394213f654a8a9362dece

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,13 +4,18 @@ class PostsController < ApplicationController
   def show
   end
 
-  def edit
+  def edit 
+    @post = Post.find(params[:id])
   end
 
   def update
-    @post.update(post_params)
+    @post.update(post_params) 
+    if @post.update(post_params)
 
-    redirect_to post_path(@post)
+      redirect_to post_path(@post)
+    else 
+      render :edit
+    end 
   end
 
   private
